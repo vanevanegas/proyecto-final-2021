50f1c22ae2fc77b81305ab06698dda8b4ea68404
root <20180549@esen.edu.sv>
2021-03-29 12:37:32 -0600

    init files

---
+++ static/main.js
@@ -0,0 +1,94 @@
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+// Open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+/*
+***** login functionality begin
+*/
+function checkLogin() {
+
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("passw").value;
+
+    var userArray = JSON.parse(localStorage.getItem("lUserArray"));
+
+    if (user !== null && user !== "") {
+        if (password !== null && password !== "") {
+
+            var canLogin = checkLoginInfo(user, password, userArray);
+            if (canLogin === true) {
+                //need a method to get the role and send it into createSessionUser below
+                var role = getUserRole(user, password, userArray)
+                createSessionUser(user, password, role)
+                window.location.href = "http://localhost:5000/dashboard";
+                //window.location.href = "http://heroku:5000/dashboard";
+            } else {
+                alert("user or password are not correct");
+            }
+
+        } else {
+            alert("password must not be empty");
+        }
+    } else {
+        alert("user must not be empty");
+    }
+
+}
+
+function checkLoginInfo(user, password, userArray) {
+    if (userArray !== null && userArray.length > 0) {
+        for (var i = 0; i < userArray.length; i++) {
+            if (userArray[i].user === user && userArray[i].password === password) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function getUserRole(pUser, pPassword, pUserArray) {
+    var role = ""
+    if (pUserArray !== null && pUserArray.length > 0) {
+        var length = pUserArray.length
+        for (var i = 0; i < length; i++) {
+            if (pUserArray[i].user === pUser && pUserArray[i].password === pPassword) {
+                role = pUserArray[i].role
+                break
+            }
+        }
+    }
+    return role
+}
+
+function createSessionUser(user, password, role) {
+    var logged_user = {
+        user: user,
+        password: password,
+        role: role
+    };
+
+    sessionStorage.setItem("loggedUser", JSON.stringify(logged_user));
+}
+
+/*
+***** login functionality end 
+*/
